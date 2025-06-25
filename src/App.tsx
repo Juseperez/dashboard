a9a9ec8c4f0a5704c78bac224bbf722b9602ff38
Juseperez <152338766+Juseperez@users.noreply.github.com>
2025-06-25 18:33:05 +0000

    Guia13

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'*/
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
   /*const [count, setCount] = useState(0)*/
 
   return (
-      <Grid container spacing={5} justifyContent="center" alignItems="center">
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+      {/* Encabezado */}
+      <HeaderUI />
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
-         {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }}
+        container justifyContent="right" alignItems="center">
 
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+        <AlertUI description="No se preveen lluvias" />Elemento: Alertas</Grid>
 
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
 
-         {/* Gráfico */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Gráfico</Grid>
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Tabla</Grid>
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Gráfico</Grid>
 
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Tabla</Grid>
 
-      </Grid>
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+    </Grid>
   )
 }
 
