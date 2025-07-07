89b57495944bedd3eb61ba3eaaa1816a61b9372a
Juseperez <152338766+Juseperez@users.noreply.github.com>
2025-07-07 19:22:59 +0000

    Guia15

---
+++ src/functions/DataFetcher.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import type { OpenMeteoResponse } from '../types/DashboardTypes';
+
+interface DataFetcherOutput {
+    data: OpenMeteoResponse | null;
+    loading: boolean;
+    error: string | null;
+}
+
+function DataFetcher(): DataFetcherOutput {
+    const [data, setData] = useState<OpenMeteoResponse | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=America%2FChicago';
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Error en la petición: ${response.status}`);
+                }
+                const result: OpenMeteoResponse = await response.json();
+                setData(result);
+            } catch (err: any) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError("Ocurrió un error desconocido al obtener los datos.");
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { data, loading, error };
+}
+
+export default DataFetcher;
